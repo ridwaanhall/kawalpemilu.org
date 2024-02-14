d3d2bba3f34b9f9a3bb0680ff1dcc590bd887139
ridwaanhall <erbyl14@gmail.com>
2024-02-14 17:52:25 +0700

    report TPS

---
+++ base/report.py
@@ -0,0 +1,27 @@
+url = "https://us-central1-kp24-fd486.cloudfunctions.net/hierarchy2"
+
+payload = {
+    "data": 
+        {
+            "id": ""
+        }
+    }
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9,mt;q=0.8",
+    "Cache-Control": "no-cache",
+    "Content-Type": "application/json",
+    "Dnt": "1",
+    "Origin": "https://kawalpemilu.org",
+    "Pragma": "no-cache",
+    "Referer": "https://kawalpemilu.org/",
+    "Sec-Ch-Ua": "\"Not A(Brand\";v=\"99\", \"Microsoft Edge\";v=\"121\", \"Chromium\";v=\"121\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+}
